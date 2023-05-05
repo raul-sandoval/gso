5c988afa09f788c3a5f45af7adc580f2f6ea3f72
Raul Sandoval <raul.sandoval@expeditors.com>
2023-05-04 21:53:43 -0600

    new: add base html format

--- webapps/index.py
+++ webapps/index.py
@@ -1,9 +1,12 @@
 ########## Imports [START] ##########
+from flask import render_template
+from flask import flash
 from webapps import app
 ########## Imports  [END]  ##########
 
 ########## @app.route("/") route [START] ##########
 @app.route("/")
 def index():
-    return 'home page'
+    flash(message='Loading data',category='info')
+    return render_template('index.html')
 ########## @app.route("/") route  [END]  ##########
