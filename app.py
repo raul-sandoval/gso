859011838a8b727b30c3cf2b90ef224a4c70d8e3
Raul Sandoval <raul.sandoval@expeditors.com>
2023-05-04 20:46:26 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+########## Imports [START] ##########
+from webapps import app
+########## Imports  [END]  ##########
+
+########## Main function [START] ########## 
+if __name__ == '__main__':
+    app.run()
+########## Main function  [END]  ########## 
---
+++ webapps/__init__.py
@@ -0,0 +1,10 @@
+########## Imports [START] ##########
+from flask import Flask
+from flask_session import Session
+########## Imports  [END]  ##########
+
+########## Setup app [START] ##########
+app = Flask(__name__)
+app.config.from_object('config')
+Session(app)
+########## Setup app  [END]  ##########
