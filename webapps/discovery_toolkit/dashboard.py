e38c9c6a6773b107ddec31c712dda84f9d3aca60
Raul Sandoval <raul.sandoval@expeditors.com>
2023-05-06 00:00:15 -0600

    update: random data for meetings and opps

--- webapps/discovery_toolkit/dashboard.py
+++ webapps/discovery_toolkit/dashboard.py
@@ -17,16 +17,30 @@ def dashboard():
 
     duns_branches = []
     gci_matches = []
-    gci_no_matches = []
-    for j in range(10):
+    gci_no_matches = []    
+    for _ in range(10):
         duns_branches.append(''.join(fake.random_uppercase_letter() for i in range(3)))
-        gci_matches.append(fake.random_int(min=0, max=560, step=3))
-        gci_no_matches.append(fake.random_int(min=0, max=560, step=4))
+        gci_matches.append(fake.random_int(min=0, max=560, step=1))
+        gci_no_matches.append(fake.random_int(min=0, max=560, step=2))
+
+    meeting_branches = []
+    total_meetings = []
+    for _ in range(20):
+        meeting_branches.append(''.join(fake.random_uppercase_letter() for i in range(3)))
+        total_meetings.append(fake.random_int(min=0, max=768, step=3))
+
+    active_won_opps_branches = []
+    won_opportunities = []
+    active_opportunities = []
+    for _ in range(20):
+        active_won_opps_branches.append(''.join(fake.random_uppercase_letter() for i in range(3)))
+        won_opportunities.append(fake.random_int(min=0, max=200, step=4))
+        active_opportunities.append(fake.random_int(min=0, max=200, step=5))
 
     response = {
         'global_engagement': {
             'account_name' : fake.company(),
-            'account_id' : fake.bothify(text='C#######'),
+            'account_id' : fake.bothify(text='C000####'),
             'is_new_logo' : ('Yes' if fake.boolean(chance_of_getting_true=25) else 'No'),
             'is_DNA' : ('Yes' if fake.boolean(chance_of_getting_true=60) else 'No'),
             'is_GNA' : ('Yes' if fake.boolean(chance_of_getting_true=50) else 'No'),
@@ -35,22 +49,31 @@ def dashboard():
             'is_Account_Management' : ('Yes' if fake.boolean(chance_of_getting_true=50) else 'No'),
         },
         'duns_location': {
-            'total_gci_match' : fake.random_int(min=1000, max=3500,step=1),
-            'total_locations' : fake.random_int(min=3500, max=5500,step=2),
-            'chart':{
+            'total_gci_match' : fake.random_int(min=1000, max=3500,step=6),
+            'total_locations' : fake.random_int(min=3500, max=5500,step=7),
+            'duns_location_chart' : {
                 'branches': duns_branches,
                 'gci_matches': gci_matches,
                 'gci_no_matches': gci_no_matches,
             },
         },
-        'meetings_information': {
-            'total_in_person_meetings': fake.random_int(min=0, max=345,step=5),
-            'total_in_phone_web_meetings': fake.random_int(min=0, max=478,step=6),
-            'total_won_opportunities': fake.random_int(min=0, max=150,step=7),
-            'total_active_opportunities': fake.random_int(min=0, max=265,step=8),
-            'total_on_hold_opportunities': fake.random_int(min=0, max=40,step=9),
-            'total_lost_opportunities': fake.random_int(min=0, max=206,step=10),
-        }
+        'meetings_information' : {
+            'total_in_person_meetings': fake.random_int(min=0, max=345,step=8),
+            'total_in_phone_web_meetings': fake.random_int(min=0, max=478,step=9),
+            'total_won_opportunities': fake.random_int(min=0, max=150,step=10),
+            'total_active_opportunities': fake.random_int(min=0, max=265,step=11),
+            'total_on_hold_opportunities': fake.random_int(min=0, max=40,step=12),
+            'total_lost_opportunities': fake.random_int(min=0, max=206,step=13),
+            'meetings_activity_chart' : {
+                'branches' : meeting_branches,
+                'total_meetings' : total_meetings,
+            },
+            'active_won_opps_chart' : {
+                'branches' : active_won_opps_branches,
+                'won_opportunities' : won_opportunities,
+                'active_opportunities' : active_opportunities,
+            },
+        },
     }
 
     flash(message='Loading data',category='info')
