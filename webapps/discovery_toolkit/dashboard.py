e52aff69799eece4da0225f003345b64884fd373
Raul Sandoval <raul.sandoval@expeditors.com>
2023-05-05 21:41:06 -0600

    update: generate random data from global engagement

--- webapps/discovery_toolkit/dashboard.py
+++ webapps/discovery_toolkit/dashboard.py
@@ -1,4 +1,5 @@
 ########## Imports [START] ##########
+from faker import Faker
 from flask import flash
 from flask import render_template
 from webapps.discovery_toolkit import APP_NAME
@@ -12,7 +13,19 @@ from webapps.discovery_toolkit import discovery_toolkit_blueprint
 ########## @discovery_toolkit_blueprint.route("/dashboard",methods=['GET','POST']) [START] ##########
 @discovery_toolkit_blueprint.route("/dashboard",methods=['GET','POST'])
 def dashboard():
-    response = {}
+    fake = Faker()
+    response = {
+        'global_engagement': {
+            'account_name': fake.company(),
+            'account_id': fake.bothify(text='C#######'),
+            'is_new_logo': ('Yes' if fake.boolean(chance_of_getting_true=25) else 'No'),
+            'is_DNA': ('Yes' if fake.boolean(chance_of_getting_true=60) else 'No'),
+            'is_GNA': ('Yes' if fake.boolean(chance_of_getting_true=50) else 'No'),
+            'is_GTDP': ('Yes' if fake.boolean(chance_of_getting_true=25) else 'No'),
+            'is_ADP': ('Yes' if fake.boolean(chance_of_getting_true=10) else 'No'),
+            'is_Account_Management': ('Yes' if fake.boolean(chance_of_getting_true=50) else 'No'),
+        },
+    }
 
     flash(message='Loading data',category='info')
     return render_template(APP_NAME + '/dashboard.html',response=response)
